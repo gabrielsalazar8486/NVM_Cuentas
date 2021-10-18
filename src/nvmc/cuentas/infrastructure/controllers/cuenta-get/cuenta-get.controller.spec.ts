271c48dbe441b8df9eafc428bffb3b3112e18637
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2021-10-18 17:49:53 -0400

    probar dokerizacion

--- src/nvmc/cuentas/infrastructure/controllers/cuenta-get/cuenta-get.controller.spec.ts
+++ src/nvmc/cuentas/infrastructure/controllers/cuenta-get/cuenta-get.controller.spec.ts
@@ -1,19 +1,42 @@
+import * as request from 'supertest'
 import { Test, TestingModule } from '@nestjs/testing';
 import { CuentaGetController } from './cuenta-get.controller';
 import {CuentasInfrastructureModule} from "../../cuentasInfrastructure.module";
+import {INestApplication} from "@nestjs/common";
 
 describe('CuentaGetController', () => {
-  let controller: CuentaGetController;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports:[CuentasInfrastructureModule],
     }).compile();
 
-    controller = module.get<CuentaGetController>(CuentaGetController);
+    app = module.createNestApplication();
+    await app.init();
+
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it(`/GET cuentas findOne`, () => {
+    return request(app.getHttpServer())
+        .get('/cuentas/1')
+        .expect(200)
+        .expect({
+          mensage: 'getCuenta',
+          id: '1',
+          response: {
+            id: '1',
+            codigo: '1.1.1',
+            nombre: 'Gabriel',
+            saldo: 100.2,
+            createAt: '2021-09-09T15:31:11.312Z',
+            updateAt: '2021-09-09T15:31:11.312Z'
+          }
+        });
   });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
 });
